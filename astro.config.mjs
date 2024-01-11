842eb40f89927b91f6f8492aeec80ee2070b3a87
Gregor Obreza <gobreza@gmail.com>
2024-01-11 13:16:25 +0100

    fixed base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://gregorobreza.github.io/astro-redirect-issue',
+    base: '/astro-redirect-issue',
     redirects: {
         '/redirected': '/new-page'
       }
